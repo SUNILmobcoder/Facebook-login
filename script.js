7a8f55c975159c66f5cc179f0e3ac6f26045dbe1
SUNILmobcoder <86093794+SUNILmobcoder@users.noreply.github.com>
2021-07-20 11:42:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@
 // Google singIn function
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    // console.log('Name: ' + profile.getName());
-    // console.log('Image URL: ' + profile.getImageUrl());
-    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
     $(".img").attr(src, profile.getImageUrl());
     $(".name").text(profile.getName());
